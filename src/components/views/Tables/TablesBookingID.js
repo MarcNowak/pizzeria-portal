49ca36dd2a22accc269c6f84ed9f4c745844dd2e
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-12-29 19:36:44 +0100

    Add 21.3 Views and routing v6

--- src/components/views/Tables/TablesBookingID.js
+++ src/components/views/Tables/TablesBookingID.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Tables.module.scss';
+import PropTypes from 'prop-types';
 
-const TablesBookingID = () => (
+const TablesBookingID = (props) => (
   <div className={styles.component}>
     <h2>Tables - Booking ID</h2>
-
+    {props.match.params.id}
   </div>
 );
+
+TablesBookingID.propTypes = {
+  match: PropTypes.any,
+
+};
 
 export default TablesBookingID;
