1c30fe4f59291618599ecb4451beb52b6cf46504
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-12-31 12:04:32 +0100

    Add 22.2 Redux deployment- task preparing  v1

--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Waiter.module.scss';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,97 +8,115 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
 
-const demoContent = [
-  { id: '1', status: 'free', idle: '0:00', order: null },
-  { id: '2', status: 'thinking', idle: '5:35', order: null },
-  { id: '3', status: 'ordered', idle: '6:22', order: 123 },
-  { id: '4', status: 'prepared', idle: '10:29', order: 234 },
-  { id: '5', status: 'delivered', idle: '15:05', order: 345 },
-  { id: '6', status: 'paid', idle: '1:07', order: 456 },
-];
-
-const renderActions = status => {
-  switch (status) {
-    case 'free':
-      return (
-        <>
-          <Button>thinking</Button>
-          <Button>new order</Button>
-        </>
-      );
-    case 'thinking':
-      return (
-        <Button>new order</Button>
-      );
-    case 'ordered':
-      return (
-        <Button>prepared</Button>
-      );
-    case 'prepared':
-      return (
-        <Button>delivered</Button>
-      );
-    case 'delivered':
-      return (
-        <Button>paid</Button>
-      );
-    case 'paid':
-      return (
-        <Button>free</Button>
-      );
-    default:
-      return null;
+class Waiter extends React.Component {
+  static propTypes = {
+    tables: PropTypes.any,
+    fetchTables: PropTypes.func,
+    loading: PropTypes.shape({
+      active: PropTypes.bool,
+      error: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    }),
   }
-};
 
-const Waiter = () => (
-  <div>
-    <div className={styles.component}>
-      <h2>Waiter</h2>
-      <Link to={`${process.env.PUBLIC_URL}/waiter/order/new`} activeClassName='active'>New Order </Link>
-      <Link to={`${process.env.PUBLIC_URL}/waiter/order/:id`} activeClassName='active'>New Order ID</Link>
-    </div>
-    <Paper className={styles.component}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Table</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Idle Time</TableCell>
-            <TableCell>Order</TableCell>
-            <TableCell>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {demoContent.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell>
-                {row.status}
-              </TableCell>
-              <TableCell>
-                {row.idle}
-              </TableCell>
-              <TableCell>
-                {row.order && (
-                  <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
-                    {row.order}
-                  </Button>
-                )}
-              </TableCell>
-              <TableCell>
-                {renderActions(row.status)}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  </div>
-);
+  componentDidMount() {
+    const { fetchTables } = this.props;
+    fetchTables();
+  }
+
+  renderActions(status) {
+    switch (status) {
+      case 'free':
+        return (
+          <>
+            <Button>thinking</Button>
+            <Button>new order</Button>
+          </>
+        );
+      case 'thinking':
+        return (
+          <Button>new order</Button>
+        );
+      case 'ordered':
+        return (
+          <Button>prepared</Button>
+        );
+      case 'prepared':
+        return (
+          <Button>delivered</Button>
+        );
+      case 'delivered':
+        return (
+          <Button>paid</Button>
+        );
+      case 'paid':
+        return (
+          <Button>free</Button>
+        );
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    const { loading: { active, error }, tables } = this.props;
+
+    if (active || !tables.length) {
+      return (
+        <Paper className={styles.component}>
+          <p>Loading...</p>
+        </Paper>
+      );
+    } else if (error) {
+      return (
+        <Paper className={styles.component}>
+          <p>Error! Details:</p>
+          <pre>{error}</pre>
+        </Paper>
+      );
+    } else {
+      return (
+        <Paper className={styles.component}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Table</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Idle Time</TableCell>
+                <TableCell>Order</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tables.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell>
+                    {row.status}
+                  </TableCell>
+                  <TableCell>
+                    {row.idle}
+                  </TableCell>
+                  <TableCell>
+                    {row.order && (
+                      <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                        {row.order}
+                      </Button>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {this.renderActions(row.status)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
+    }
+  }
+}
 
 export default Waiter;
